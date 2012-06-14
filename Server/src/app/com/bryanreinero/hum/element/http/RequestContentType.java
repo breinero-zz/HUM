9b05736c7119bc8ec89aeddfd53cc163f9fc97b0
Bryan Reinero <breinero@gmail.com>
2012-06-13 22:12:38 -0700

    Added missing java files which are needed for the last major commit to work

---
+++ Server/src/app/com/bryanreinero/hum/element/http/RequestContentType.java
@@ -0,0 +1,18 @@
+package com.bryanreinero.hum.element.http;
+
+import com.bryanreinero.hum.element.HumElement;
+import com.bryanreinero.hum.visitor.Visitor;
+
+public class RequestContentType extends HumElement {
+
+	@Override
+	public void accept(Visitor visitor) {
+		visitor.visit(this);
+	}
+
+	@Override
+	public void addParent(HumElement element) {
+		element.addChild(this);
+	}
+
+}
